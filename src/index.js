29df8c7e083ebbe757f0caf3f628e2473283b3ea
uNicom <mokritsa@bk.ru>
2022-02-06 19:27:27 +0300

    finish task brackets

---
+++ src/index.js
@@ -0,0 +1,14 @@
+module.exports = function check(str, bracketsConfig) {
+  let arr = [...str];
+  for(let i = 0; i < arr.length; i++) 
+      for(let j=0; j<bracketsConfig.length; j++)
+          if(arr[i] == bracketsConfig[j][0] && arr[i + 1] == bracketsConfig[j][1]){
+              arr.splice(i, 2);
+              i = 0;
+              j = -1;
+          }
+  if (arr.length == 0)
+      return true;
+  else
+      return false;
+}
